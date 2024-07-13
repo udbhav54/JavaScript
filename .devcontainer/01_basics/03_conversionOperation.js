88813742f65e69b91564049520459bdd3f2aa0cb
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-07-13 15:07:21 +0000

    Updated one

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -2,30 +2,67 @@
 // let score = null
 // let score = undefined
 // let score = false
- let score = "udbhav"
+//  let score = "udbhav"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33" => 33
 // "33abc" =>NaN not a number
 // true =>1; false => 0
 
-let isLoggedIn = "udbhav"
+// let isLoggedIn = "udbhav"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
 // "udbhav" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log( typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log( typeof stringNumber)
+
+/************************* Operations **************************/
+
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 2) // power
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2 % 3)
+
+// let str1 = "hii"
+// let str2 = " udbhav"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2"); //12
+// console.log("1" + 2); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+// console.log((3 + 4) * 5 % 3);
+// console.log(true) //true
+// console.log(+true); // 1
+// // console.log(true+);// not possible
+// console.log(+"")//0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
