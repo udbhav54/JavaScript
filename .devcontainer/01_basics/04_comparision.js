9651e16ae0c851f13b1c72e4438b5ece9bf3068e
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-07-13 15:30:01 +0000

    comparision

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // true
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+/// all false
+
+// ===
+console.log("2" === 2); // false
+
+// we need to avoid this kind of conversion in future
