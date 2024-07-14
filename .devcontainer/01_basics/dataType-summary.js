12780d299b4f22d8835b45eea20f2ac046d2a906
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-07-14 07:07:06 +0000

    DataType_Summary

---
+++ .devcontainer/01_basics/dataType-summary.js
@@ -0,0 +1,38 @@
+// Primitive (call by value)
+
+// 7 types : String, Number, Boolean, null, Undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3 //  number
+
+const isLoggedIn = false // boolean
+const outsideTemp = null // object
+let userEmail; // undefined
+const myName = "Udbhav" //String
+
+const id = Symbol('123') // symbol
+const anotherId = Symbol('123')
+
+console.log( id === anotherId)
+
+const bigNumber = 23268454451654451n //bigint
+
+
+// Reference(Non Primitive) 
+
+// Array, Object, Functions
+// dataType (Array , object) => function & Function => Object Function
+
+const heros = ["spiderman", "batman", "ironman"]  // object
+
+let myObj = {
+    name : "udbhav",
+    age: 22,
+} // object
+
+const myFunction = function(){
+    console.log("Hello World");
+
+} // function
+
+console.log(typeof myName);
