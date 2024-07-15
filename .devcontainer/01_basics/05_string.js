b83aea5ceb7fcd6235788f8d8ea7a2f9c4989dde
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-07-15 17:04:49 +0000

    String

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,19 @@
+const name = "udbhav"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value"); outdated
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('udbhav');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('v'));
+
+//  const newString = gameName.substring(0,3) // last value not included
+//  console.log(newString);
