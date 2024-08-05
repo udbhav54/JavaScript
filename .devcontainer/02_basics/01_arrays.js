94454b54bc62668daeb9b286eeef602603b667d3
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-08-05 09:17:12 +0000

    Array_part1

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4]
+const myHeros = ["shaktiman", "naagraj"]
+const myArr2 = new Array(1,2,3,4)
+
+// console.log(myArr[0]);
+
+
+// Array methode
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop() // remove last value
+
+// myArr.unshift(9) // insert at first index
+// myArr.shift() // to remove at first index
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( typeof newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 2)
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 2)
+console.log(myn2);
+console.log("C ", myArr);
+
+
+
+
+
