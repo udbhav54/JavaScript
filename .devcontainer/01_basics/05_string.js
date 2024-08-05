adbd2731c94362bd1ca5f66530a9b2561a01bfa8
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-08-05 07:41:38 +0000

    String_Updated

--- .devcontainer/01_basics/05_string.js
+++ .devcontainer/01_basics/05_string.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('udbhav');
+const gameName = new String('udbha-v');
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
@@ -17,3 +17,19 @@ console.log(gameName.length);
 
 //  const newString = gameName.substring(0,3) // last value not included
 //  console.log(newString);
+
+const anotherString = gameName.slice(-5,3) // we can give negative value
+console.log(anotherString);
+
+
+const newStringOne = "  udbhav  "
+console.log(newStringOne);
+console.log(newStringOne.trim()); // to remove extra character work on whiteSpace and new line
+
+
+const url = "https://hitest.com/hitest%20choudhary"
+console.log(url.replace('%20','-'));
+
+console.log(gameName.split('-'));
+
+
