f2069a8c7b8a21287534512b85fd6f6c71f1a8fc
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-08-10 08:57:13 +0000

    HigherLoop_Part2

---
+++ .devcontainer/05_iterations/04_loop.js
@@ -0,0 +1,21 @@
+const myObject = {
+    js : "javascreipt",
+    cpp : "c++",
+    rb : "reby",
+    swift : "swift"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]} `);
+    
+}
+
+const myArray = ["js", "cpp", "rb", "py"]
+
+for (const key in myArray) {
+    
+    console.log(`${key} shortcut is for ${myArray[key]} `);
+        
+    }
+
+    // map not iterable
