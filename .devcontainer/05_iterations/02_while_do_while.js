855679bad53f3b650ce755687dedf2ff82c5e66b
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-08-10 06:51:38 +0000

    While_Do_While

---
+++ .devcontainer/05_iterations/02_while_do_while.js
@@ -0,0 +1,24 @@
+// while
+
+let index = 1
+while (index < 20) {
+    // console.log(index);
+    index += 5
+}
+
+let myArray = ["flash", "batman", "superman"]
+let arr = 0 
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr += 1
+}
+
+// do_while
+
+let score = 100
+
+do {
+    console.log(`score is ${score}`);
+    score += 100
+    
+} while (score <= 1000);
