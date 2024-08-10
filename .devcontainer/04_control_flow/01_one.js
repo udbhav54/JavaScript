2fce5383c588063562ace887fb29a10b65c3c156
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-08-10 05:30:48 +0000

    IF - Statement

---
+++ .devcontainer/04_control_flow/01_one.js
@@ -0,0 +1,33 @@
+// if
+const temp = 40
+// <,>,<=,>=,==,===,!=,!==
+if (temp === 40) {
+    // console.log(`temperature is greater than 35`);
+}
+
+// if (temp > 50) {
+//     console.log(`temp greater than 50`);
+    
+// } else {
+//     console.log(`teomp is  ${temp}`);
+// }
+
+const balance = 1000
+// if (balance > 500) console.log(`test`),console.log(`test2`);
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2 === 3) {
+    // console.log(`Allow to buy course`);
+    
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    // console.log(`Allow to buy course1`);
+}
+
+// in this way we can apply nested if-else
+
