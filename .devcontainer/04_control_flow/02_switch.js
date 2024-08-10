a28ab7394c2061fbabca2be747bb4d61740c193b
Udbhav Kumar <udbhavkumar54@gmail.com>
2024-08-10 05:33:40 +0000

    Switch

---
+++ .devcontainer/04_control_flow/02_switch.js
@@ -0,0 +1,24 @@
+// switch
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("Febuary");
+        break
+    default:
+        console.log(`enter the valid one`);
+        
+        break;
+}
